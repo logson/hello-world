882575a14b0cd8a270525c3099b42185e3f97b6d
logson <1318004009@qq.com>
2016-03-31 22:16:14 +0800

    first commit to local

---
+++ src/com/logson/gittest/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.logson.gittest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Just for test!");
+	}
+
+}
